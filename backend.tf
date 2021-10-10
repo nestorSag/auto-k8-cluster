55c206238c4d1f26db885dac1353332d5651753b
nestorSag <nestor.sag@gmail.com>
2021-10-10 21:27:38 +0100

    tf and ansible scripts added

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region = "us-west-2"
+    key    = "terraform-state-file"
+    bucket = "devops-acg-terraformstate123"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,14 @@
+output "jenkins-main-node-public-ip" {
+  value = aws_instance.jenkins-master.public_ip
+}
+
+output "jenkins-worker-public-ips" {
+  value = {
+    for instance in aws_instance.jenkins-worker :
+    instance.id => instance.public_ip
+  }
+}
+/*
+output "lb-dns-name" {
+  value = aws_lb.application-lb.dns_name
+}*/
